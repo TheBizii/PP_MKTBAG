905a9934a6bc5285e598edd07484328db99a97dd
profek10 <aleksander.grobelnik1@gmail.com>
2022-05-23 12:28:05 +0200

    forcing https instead of http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forcescheme("https");
+        URL::forceScheme("https");
         //
     }
 }
