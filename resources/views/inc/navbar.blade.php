cda6a1fa3a9e6c7e813d4ef3fcce73439dfd030d
profek10 <aleksander.grobelnik1@gmail.com>
2022-05-23 13:30:51 +0200

    New navbar for logged in user

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,48 +1,3 @@
-<!-- Navbar
-<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-  <div class="container">
-    <a class="navbar-brand" href="/"
-      ><img
-        id="logo"
-        src="/logo.png"
-        alt="Logo"
-        draggable="false"
-        height="70"
-        width="550"
-    /></a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-mdb-toggle="collapse"
-      data-mdb-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto align-items-center">
-        <li class="nav-item">
-          <a class="nav-link mx-2" href="/ads/create"><i class="fas fa-plus-circle pe-2"></i>Create ad</a>
-        </li>
-        <php
-          session_start();
-          if(!isset($_SESSION["user"])){
-            echo "<li class=\"nav-item ms-3\">
-              <a class=\"btn btn-black btn-rounded\" href=\"login.php\">Sign in</a>
-            </li>";
-          } else {
-            echo "<li class=\"nav-item ms-3\">
-              <a class=\"btn btn-black btn-rounded\" href=\"sessionDestroy.php\">Log out</a>
-            </li>";
-          }
-        ?>
-      </ul>
-    </div>
-  </div>
-</nav>
-Navbar -->
 <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
@@ -89,16 +44,10 @@ Navbar -->
                             {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('home') }}">
+                            <a class="dropdown-item" href="{{ url('/') }}">
                                 <i class="fa fa-home"></i>
                                 {{ __('Home') }}
                             </a>
-                            @if (\Illuminate\Support\Facades\Auth::user()->is_admin)
-                                <a class="dropdown-item" href="{{ route('users.index') }}">
-                                    <i class="fa fa-user-circle"></i>
-                                    {{ __('Users') }}
-                                </a>
-                            @endif
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
