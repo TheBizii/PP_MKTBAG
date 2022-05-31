cc5688722daeb503f26c1a617a22dbd1356000a3
TheBizii <bizjaktomaz@protonmail.com>
2022-05-31 23:33:08 +0200

    PP-38 Unfinished version of lexer

--- PrePJ/src/main/kotlin/main.kt
+++ PrePJ/src/main/kotlin/main.kt
@@ -6,41 +6,41 @@ import java.util.LinkedList
 
 const val EOF_SYMBOL = -1
 const val ERROR_STATE = 0
-const val SKIP_VALUE = 0
-const val NEWLINE = '\n'.code
+const val SKIP_VALUE = ""
+
+const val NEWLINE = 10
 
 interface Automaton {
   val states: Set<Int>
   val alphabet: IntRange
   fun next(state: Int, symbol: Int): Int
-  fun value(state: Int): Int
+  fun value(state: Int): String
   val startState: Int
   val finalStates: Set<Int>
 }
 
-object MinimalisticCPP : Automaton {
-  override val states =
-    setOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30)
-  override val alphabet = 0..255
+object Example : Automaton {
+  override val states = setOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33)
+  override val alphabet = 0 .. 255
   override val startState = 1
-  override val finalStates = setOf(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 19, 21, 23, 25, 30)
+  override val finalStates = setOf(2, 4, 5, 7, 8, 11, 13, 15, 17, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33)
 
   private val numberOfStates = states.maxOrNull()!! + 1
   private val numberOfSymbols = alphabet.maxOrNull()!! + 1
-  private val transitions = Array(numberOfStates) { IntArray(numberOfSymbols) }
-  private val values: Array<Int> = Array(numberOfStates) { 0 }
+  private val transitions = Array(numberOfStates) {IntArray(numberOfSymbols)}
+  private val values: Array<String> = Array(numberOfStates) {""}
 
   private fun setTransition(from: Int, symbol: Char, to: Int) {
-    transitions[from][symbol.code] = to
+    transitions[from][symbol.toInt()] = to
   }
 
-  private fun setTransition(from: Int, range: IntRange, to: Int) {
-    for (i in range) {
-      transitions[from][i] = to
+  private fun setTransition(from: Int, symbols: IntRange, to: Int) {
+    for (symbol in symbols) {
+      transitions[from][symbol] = to
     }
   }
 
-  private fun setValue(state: Int, terminal: Int) {
+  private fun setValue(state: Int, terminal: String) {
     values[state] = terminal
   }
 
@@ -52,238 +52,221 @@ object MinimalisticCPP : Automaton {
       transitions[state][symbol]
     }
 
-  override fun value(state: Int): Int {
+  override fun value(state: Int): String {
     assert(states.contains(state))
     return values[state]
   }
 
   init {
-    //Numbers
-    setTransition(1, 48..57, 2)
-    //Characters
-    setTransition(1, 65..86, 4)
-    setTransition(1, 88..90, 4)
-    setTransition(1, 97..99, 4)
-    setTransition(1, 'e', 4)
-    setTransition(1, 103..115, 4)
-    setTransition(1, 117..122, 4)
-    //Operators
-    setTransition(1, '+', 6)
-    setTransition(1, '-', 7)
-    setTransition(1, '*', 8)
-    setTransition(1, '/', 9)
-    setTransition(1, '^', 10)
-    //Brackets
-    setTransition(1, '(', 11)
-    setTransition(1, ')', 12)
-    //Numbers
-    setTransition(2, 48..57, 2)
-    setTransition(2, '.', 3)
-    //Numbers
-    setTransition(3, 48..57, 3)
-    //Characters
-    setTransition(4, 65..90, 4)
-    setTransition(4, 97..122, 4)
-    //Numbers
-    setTransition(4, 48..57, 5)
-    //Numbers
-    setTransition(5, 48..57, 5)
+    /* RESERVED KEYWORDS */
+    setTransition(1, 'c', 2)
+    setTransition(2, 'i', 3)
+    setTransition(3, 't', 4)
+    setTransition(4, 'y', 5)
 
-    setTransition(1, ' ', 13)
-    setTransition(1, '\t', 13)
-    setTransition(1, '\n', 13)
-    setTransition(1, ':', 14)
-    setTransition(14, '=', 15)
-    setTransition(1, ';', 16)
-    setTransition(1, 'f', 17)
-    setTransition(17, 65..90, 4)
-    setTransition(17, 97..110, 4)
-    setTransition(17, 112..122, 4)
-    setTransition(17, 'o', 18)
-    setTransition(18, 65..90, 4)
-    setTransition(18, 97..113, 4)
-    setTransition(18, 115..122, 4)
-    setTransition(18, 'r', 19)
-    setTransition(1, 't', 20)
-    setTransition(20, 65..90, 4)
-    setTransition(20, 97..110, 4)
-    setTransition(20, 112..122, 4)
-    setTransition(20, 'o', 21)
-    setTransition(1, 'd', 22)
-    setTransition(22, 65..90, 4)
-    setTransition(22, 97..110, 4)
-    setTransition(22, 112..122, 4)
+    setTransition(1, 'v', 6)
+    setTransition(6, 'a', 7)
+    setTransition(7, 'r', 8)
+    setTransition(8, 'i', 9)
+    setTransition(9, 'a', 10)
+    setTransition(10, 'b', 11)
+    setTransition(11, 'l', 12)
+    setTransition(12, 'e', 13)
+    setTransition(13, 's', 14)
+
+    setTransition(1, 'e', 15)
+    setTransition(15, 's', 16)
+    setTransition(16, 't', 17)
+    setTransition(17, 'a', 18)
+    setTransition(18, 't', 19)
+    setTransition(19, 'e', 20)
+    setTransition(20, 's', 21)
+
+    setTransition(1, 'r', 22)
     setTransition(22, 'o', 23)
-    setTransition(23, 65..90, 4)
-    setTransition(23, 97..109, 4)
-    setTransition(23, 111..122, 4)
-    setTransition(23, 'n', 24)
-    setTransition(24, 65..90, 4)
-    setTransition(24, 97..110, 4)
-    setTransition(24, 112..122, 4)
-    setTransition(24, 'e', 25)
-    setTransition(1, 'W', 26)
-    setTransition(26, 65..81, 4)
-    setTransition(26, 82..90, 4)
-    setTransition(26, 97..122, 4)
-    setTransition(26, 'R', 27)
-    setTransition(27, 65..72, 4)
-    setTransition(27, 74..90, 4)
-    setTransition(27, 97..122, 4)
-    setTransition(27, 'I', 28)
-    setTransition(28, 65..83, 4)
-    setTransition(28, 85..90, 4)
-    setTransition(28, 97..122, 4)
-    setTransition(28, 'T', 29)
-    setTransition(29, 65..68, 4)
-    setTransition(29, 70..90, 4)
-    setTransition(29, 97..122, 4)
-    setTransition(29, 'E', 30)
-    //Float
-    setValue(2, 1);
-    setValue(3, 1);
-    //Variable
-    setValue(4, 2);
-    setValue(5, 2);
-    //Operator
-    setValue(6, 3);
-    setValue(7, 4);
-    setValue(8, 5);
-    setValue(9, 6);
-    setValue(10, 7);
-    setValue(11, 8);
-    setValue(12, 9);
-    setValue(15, 10);
-    setValue(16, 11);
-    setValue(19, 12);
-    setValue(21, 13);
-    setValue(23, 14);
-    setValue(25, 15);
-    setValue(30, 16);
+    setTransition(23, 'a', 24)
+    setTransition(24, 'd', 25)
+    setTransition(25, 's', 26)
+
+    setTransition(1, 'L', 27)
+    setTransition(27, 'i', 28)
+    setTransition(28, 'n', 29)
+    setTransition(29, 'e', 30)
+
+    setTransition(1, 'p', 31)
+    setTransition(31, 'o', 32)
+    setTransition(32, 'i', 33)
+    setTransition(33, 'n', 34)
+    setTransition(34, 't', 35)
+
+    setTransition(1, 'P', 36)
+    setTransition(36, 'o', 37)
+    setTransition(37, 'l', 38)
+    setTransition(38, 'y', 39)
+    setTransition(39, 'g', 40)
+    setTransition(40, 'o', 41)
+    setTransition(41, 'n', 42)
+
+    setTransition(1, 'a', 57)
+    setTransition(57, 's', 58)
+    /* END OF RESERVED KEYWORDS */
+
+    /* SYMBOLS */
+    setTransition(1, '-', 43)
+    setTransition(43, '>', 44)
+
+    setTransition(1, '{', 45)
+
+    setTransition(1, '}', 46)
+
+    setTransition(1, '[', 47)
+
+    setTransition(1, ']', 48)
+
+    setTransition(1, '(', 59)
+
+    setTransition(1, ')', 60)
+    /* END OF SYMBOLS */
+
+    /* FLOAT */
+    setTransition(1, 48..57, 50)
+    setTransition(50, 48..57, 50)
+    setTransition(50, '.', 51)
+    setTransition(51, 48..57, 52)
+    setTransition(52, 48..57, 52)
+    /* END OF FLOAT */
+
+    /* STRING */
+    setTransition(1, '"', 53)
+    setTransition(53, '"', 54)
+    setTransition(53, 0..9, 55)
+    setTransition(53, 11..12, 55)
+    setTransition(53, 14..33, 55)
+    setTransition(53, 35..127, 55)
+    setTransition(55, 0..9, 55)
+    setTransition(55, 11..12, 55)
+    setTransition(55, 14..33, 55)
+    setTransition(55, 35..127, 55)
+    setTransition(55, '"', 56)
+    /* END OF STRING */
+
+    /* COMMA */
+    setTransition(1, ',', 49)
+    /* END OF COMMA */
+
+    /* VARIABLE IDENTIFIER */
+    setTransition(1, 65..75, 61)
+    setTransition(1, 77..79, 61)
+    setTransition(1, 81..90, 61)
+    setTransition(1, 'b', 61)
+    setTransition(1, 'd', 61)
+    setTransition(1, 102..111, 61)
+    setTransition(1, 'q', 61)
+    setTransition(1, 115..117, 61)
+    setTransition(1, 119..122, 61)
+    setTransition(1, '_', 61)
+    setTransition(61, 65..90, 61)
+    setTransition(61, 97..122, 61)
+    setTransition(61, 48..57, 61)
+    setTransition(61, '_', 61)
+
+    // c[^i]ty
+    setTransition(2, 65..90, 61)
+    setTransition(2, 97..104, 61)
+    setTransition(2, 106..122, 61)
+    setTransition(2, 48..57, 61)
+    setTransition(2, '_', 61)
+    // ci[^t]y
+    setTransition(3, 65..90, 61)
+    setTransition(3, 97..115, 61)
+    setTransition(3, 117..122, 61)
+    setTransition(3, 48..57, 61)
+    setTransition(3, '_', 61)
+    // cit[^y]
+    setTransition(4, 65..90, 61)
+    setTransition(4, 97..120, 61)
+    setTransition(4, 'z', 61)
+    setTransition(4, 48..57, 61)
+    setTransition(4, '_', 61)
+    // v[^a]riables
+    setTransition(6, 65..90, 61)
+    setTransition(6, 98..122, 61)
+    setTransition(6, 48..57, 61)
+    setTransition(6, '_', 61)
+    // va[^r]iables
+    setTransition(7, 65..90, 61)
+    setTransition(7, 97..113, 61)
+    setTransition(7, 115..122, 61)
+    setTransition(7, 48..57, 61)
+    setTransition(7, '_', 61)
+    // var[^i]ables
+    setTransition(8, 65..90, 61)
+    setTransition(8, 97..104, 61)
+    setTransition(8, 106..122, 61)
+    setTransition(8, 48..57, 61)
+    setTransition(8, '_', 61)
+    // vari[^a]bles
+    setTransition(9, 65..90, 61)
+    setTransition(9, 98..122, 61)
+    setTransition(9, 48..57, 61)
+    setTransition(9, '_', 61)
+    // varia[^b]les
+    setTransition(10, 65..90, 61)
+    setTransition(10, 'a', 61)
+    setTransition(10, 99..122, 61)
+    setTransition(10, 48..57, 61)
+    setTransition(10, '_', 61)
+    // variab[^l]es
+    setTransition(11, 65..90, 61)
+    setTransition(11, 97..107, 61)
+    setTransition(11, 109..122, 61)
+    setTransition(11, 48..57, 61)
+    setTransition(11, '_', 61)
+    // variabl[^e]s
+    setTransition(12, 65..90, 61)
+    setTransition(12, 97..100, 61)
+    setTransition(12, 102..122, 61)
+    setTransition(12, 48..57, 61)
+    setTransition(12, '_', 61)
+    // variable[^s]
+    setTransition(13, 65..90, 61)
+    setTransition(13, 97..114, 61)
+    setTransition(13, 116..122, 61)
+    setTransition(13, 48..57, 61)
+    setTransition(13, '_', 61)
+    // e[^s]tates
+    setTransition(15, 65..90, 61)
+    /* END OF VARIABLE IDENTIFIER */
+
+    setValue(2, "float")
+    setValue(4, "float")
+    setValue(23, "variable")
+    setValue(24, "variable")
+    setValue(5, "plus")
+    setValue(25, "minus")
+    setValue(26, "times")
+    setValue(27, "divide")
+    setValue(28, "pow")
+    setValue(29, "lparen")
+    setValue(30, "rparen")
+    setValue(31, "semi")
+    setValue(7, "assign")
+    setValue(11, "for")
+    setValue(13, "do")
+    setValue(15, "done")
+    setValue(17, "to")
+    setValue(22, "write")
   }
 }
 
-data class Token(val value: Int, val lexeme: String, val startRow: Int, val startColumn: Int)
+data class Token(val value: String, val lexeme: String, val startRow: Int, val startColumn: Int)
 
-class Scanner(private val automaton: Automaton, private val stream: InputStream, private val debug: Boolean) {
+class Scanner(private val automaton: Automaton, private val stream: InputStream) {
   private var state = automaton.startState
   private var last: Int? = null
   private var buffer = LinkedList<Byte>()
   private var row = 1
   private var column = 1
-  private var prevOperator = Token(-1, "EOF", row, column)
-  private var token = Token(-1, "EOF", row, column)
-
-  init {
-    try {
-      nextToken()
-      A()
-      if (name(token.value) != "EOF") throw Error("reject")
-      println("accept")
-    } catch (err: Error) {
-      println(err.message)
-    }
-  }
-
-  private fun A() {
-    if (debug) println("A")
-    if (name(token.value) == "write")
-      CA()
-    else if (name(token.value) == "variable")
-      BA()
-    else if (name(token.value) == "for") {
-      D()
-    }
-  }
-
-  private fun D() {
-    if (debug) println("D")
-    nextToken()
-    A()
-    if (name(token.value) == "to") {
-      nextToken()
-      CB()
-      if (debug) println(name(token.value))
-      if (name(token.value) == "do") {
-        nextToken()
-        A()
-        if (debug) println(name(token.value))
-        if (name(token.value) == "done") {
-          nextToken()
-          S()
-        } else throw Error("reject")
-      } else throw Error("reject")
-    } else throw Error("reject")
-  }
-
-  private fun BA() {
-    if (debug) println("BA")
-    if (name(token.value) == "variable") {
-      nextToken()
-      BB()
-    } else if (name(token.value) == "float") {
-      nextToken()
-      BB()
-    }
-  }
-
-  private fun BB() {
-    if (debug) println("BB")
-    if (name(token.value) == "assign") {
-      nextToken()
-      CB()
-    }
-  }
-
-  private fun CA() {
-    if (debug) println("CA")
-    if (name(token.value) == "write") {
-      nextToken()
-      CB()
-    }
-  }
-
-  private fun CB() {
-    if (debug) println("CB")
-    if (name(token.value) == "float") {
-      nextToken()
-      H()
-    } else if (name(token.value) == "variable") {
-      nextToken()
-      H()
-    } else if (name(token.value) == "minus") {
-      H()
-    } else if (name(token.value) == "plus") {
-      H()
-    }
-  }
-
-  private fun H() {
-    if (debug) println("H")
-    if (name(token.value) == "times") {
-      nextToken()
-      CB()
-    } else if (name(token.value) == "divide") {
-      nextToken()
-      CB()
-    } else if (name(token.value) == "plus") {
-      nextToken()
-      CB()
-    } else if (name(token.value) == "minus") {
-      nextToken()
-      CB()
-    } else {
-      S()
-    }
-  }
-
-  private fun S() {
-    if (debug) println("S")
-    if (name(token.value) == "semi") {
-      nextToken()
-      A()
-    }
-  }
 
   private fun updatePosition(symbol: Int) {
     if (symbol == NEWLINE) {
@@ -294,7 +277,7 @@ class Scanner(private val automaton: Automaton, private val stream: InputStream,
     }
   }
 
-  private fun getValue(): Int {
+  private fun getValue(): String {
     var symbol = last ?: stream.read()
     state = automaton.startState
 
@@ -317,8 +300,8 @@ class Scanner(private val automaton: Automaton, private val stream: InputStream,
   fun eof(): Boolean =
     last == EOF_SYMBOL
 
-  private fun getToken(): Token {
-    if (eof()) return Token(-1, "EOF", row, column)
+  fun getToken(): Token? {
+    if (eof()) return null
 
     val startRow = row
     val startColumn = column
@@ -330,40 +313,19 @@ class Scanner(private val automaton: Automaton, private val stream: InputStream,
     else
       Token(value, String(buffer.toByteArray()), startRow, startColumn)
   }
+}
 
-  private fun nextToken() {
-    token = getToken()
-    if (debug) println(name(token.value))
+fun printTokens(scanner: Scanner) {
+  val token = scanner.getToken()
+  if (token != null) {
+    print("${token.value}(\"${token.lexeme}\") ")
+    printTokens(scanner)
   }
 }
 
-fun name(value: Int) =
-  when (value) {
-    -1 -> "EOF"
-    1 -> "float"
-    2 -> "variable"
-    3 -> "plus"
-    4 -> "minus"
-    5 -> "times"
-    6 -> "divide"
-    7 -> "pow"
-    8 -> "lparen"
-    9 -> "rparen"
-    10 -> "assign"
-    11 -> "semi"
-    12 -> "for"
-    13 -> "to"
-    14 -> "do"
-    15 -> "done"
-    16 -> "write"
-    else -> throw Error("Invalid value")
-  }
-
 fun main(args: Array<String>) {
-  val debug = if (args.size > 1) {
-    args[1].toBoolean();
-  } else {
-    false
+  for (file in args) {
+    val scanner = Scanner(Example, File(file).inputStream())
+    printTokens(scanner)
   }
-  Scanner(MinimalisticCPP, File(args[0]).inputStream(), debug)
 }
